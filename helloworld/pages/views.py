11ab2153d286508ff3573447c9169c7c91b5ca08
Momona-Wada <momona.9410@gmail.com>
2025-01-10 11:25:16 -0800

    Initial commit

---
+++ helloworld/pages/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.http import Http404
+from django.urls import reverse
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+def homePageView(request):
+    return render(request, "home.html")
+
+def aboutPageView(request):
+    return render(request, "about.html")
