aa047958c5252c9f4971658753837de10c899539
Momona-Wada <momona.9410@gmail.com>
2025-01-13 10:41:22 -0800

    Create result page

--- helloworld/pages/urls.py
+++ helloworld/pages/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import homePageView, aboutPageView, momonaPageView
+from .views import homePageView, aboutPageView, momonaPageView, homePost, results
 
 urlpatterns = [
     path("", homePageView, name="home"),
     path("about/",aboutPageView, name="about"),
     path("momona/", momonaPageView, name="momona"),
+    path("homePost/", homePost, name="home_post"),
+    path("<int:choice>/results/", results, name="results")
 ]
--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -6,10 +6,41 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 def homePageView(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {
+        "my_numbers":[1, 2, 3, 4, 5, 6],
+        "first_name": "Momona",
+        "last_name": "Wada"
+    })
 
 def aboutPageView(request):
     return render(request, "about.html")
 
 def momonaPageView(request):
     return render(request, "momona.html")
+
+def homePost(request):
+    # create variable to store choice that is recognized through entire function
+    choice = -999
+
+    try:
+        # Extract value from request object by control name.
+        current_choice = request.POST["choice"]
+
+        # Crude debugging effort
+        print(f"*** Years work experience: {current_choice}")
+        choice = int(current_choice)
+
+    except:
+        return render(request, "home.html", {
+            "error_message": "*** The choice was missing. Please try again ***",
+            "my_numbers": [1, 2, 3, 4, 5, 6]
+        })
+    else:
+        # always return an HttpResponseRedirect after successfully dealing with POST data.
+        # This prevents data from being posted twice if a user hits the Back button
+        return HttpResponseRedirect(reverse("results", args=(choice,)))
+
+
+def results(request, choice):
+    print("*** Inside results()")
+    return render(request, "result.html", {"choice": choice})
